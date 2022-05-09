756f09b6ba7724eba3048cdbc75210b1ac075400
marvin-w <me@marvin-wichmann.de>
2022-05-10 00:11:35 +0200

    Do not assert the version directly

--- test/conftest.py
+++ test/conftest.py
@@ -2,11 +2,15 @@
 import json
 import os
 
+from xknxproject.models import KNXProject
 
-def assert_stub(to_be_verified, stub_name: str) -> None:
+
+def assert_stub(to_be_verified: KNXProject, stub_name: str) -> None:
     """Assert input matched loaded stub file."""
     stub_path = os.path.join(os.path.dirname(__file__), "resources/stubs/" + stub_name)
 
     with open(stub_path, encoding="utf-8") as stub_file:
         stub = json.load(stub_file)
-        assert stub == to_be_verified
+        assert stub["topology"] == to_be_verified["topology"]
+        assert stub["group_addresses"] == to_be_verified["group_addresses"]
+        assert stub["devices"] == to_be_verified["devices"]
