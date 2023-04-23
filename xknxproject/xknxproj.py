baa1d3b3207a255be4b8fe28a22c9ed36d68e15f
farmio <farmio@alphart.net>
2023-04-24 00:04:30 +0200

    Add logging for parser
    and fix version string

--- xknxproject/xknxproj.py
+++ xknxproject/xknxproj.py
@@ -1,12 +1,16 @@
 """ETS Project Parser is a library to parse ETS project files."""
 from __future__ import annotations
 
+import logging
 from pathlib import Path
+import time
 
-from xknxproject import __version__
+from xknxproject.__version__ import __version__
 from xknxproject.models import KNXProject
 from xknxproject.xml import XMLParser
 from xknxproject.zip.extractor import extract
+
+_LOGGER = logging.getLogger("xknxproject.log")
 
 
 class XKNXProj:
@@ -27,5 +31,20 @@ class XKNXProj:
 
     def parse(self) -> KNXProject:
         """Parse the KNX project."""
+        _LOGGER.info(
+            'Xknxproject version %s parsing "%s" with%s password...',
+            self.version,
+            self.path,
+            "" if self.password else "out",
+        )
+        _start = time.time()
         with extract(self.path, self.password) as knx_project_content:
-            return XMLParser(knx_project_content).parse(self.language)
+            project = XMLParser(knx_project_content).parse(self.language)
+            _LOGGER.info("Parsing took %s seconds", time.time() - _start)
+            _LOGGER.info(
+                "Found %s group addresses, %s devices and %s used communication objects",
+                len(project["group_addresses"]),
+                len(project["devices"]),
+                len(project["communication_objects"]),
+            )
+            return project
