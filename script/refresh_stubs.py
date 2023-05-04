7fb48cd259b7e0b759f187654b0e2135c3c97107
farmio <farmio@alphart.net>
2023-05-04 23:50:28 +0200

    Add refresh_stubs helper

---
+++ script/refresh_stubs.py
@@ -0,0 +1,24 @@
+"""Refresh stubs for testing."""
+import json
+
+from xknxproject import XKNXProj
+
+# run from project directory
+# python3 -m script.refresh_stubs
+
+file_names = [
+    ("module-definition-test", "test", "de-DE"),
+    ("xknx_test_project", "test", None),
+]
+
+for file_name, password, language in file_names:
+    print(f"Parsing {file_name}.knxproj")
+    knxproj = XKNXProj(
+        path=f"test/resources/{file_name}.knxproj",
+        password=password,
+        language=language,
+    )
+    project = knxproj.parse()
+
+    with open(f"test/resources/stubs/{file_name}.json", "w", encoding="utf8") as f:
+        json.dump(project, f, indent=2, ensure_ascii=False)
