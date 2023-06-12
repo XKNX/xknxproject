8154b78669ab34dccf82c3fed3bf3506b25e92b6
Cyril Jaquier <cyril.jaquier@jaqpot.net>
2023-06-12 20:17:32 +0200

    Add unit tests for ETS4 .knxproj
    The tests uses a dummy project created with ETS 4.2.0 (Build 3884). The
    same project has been exported with and without a password.

--- test/xml/test_parser.py
+++ test/xml/test_parser.py
@@ -1,6 +1,8 @@
 """Test parser."""
 from test import RESOURCES_PATH
 
+import pytest
+
 from xknxproject.xml.parser import XMLParser
 from xknxproject.zip import extract
 
@@ -59,6 +61,39 @@ def test_parse_project_ets5():
     assert len(parser.areas[1].lines[1].devices) == 3
     assert len(parser.areas[1].lines[1].devices[0].additional_addresses) == 4
     assert len(parser.areas[1].lines[1].devices[1].com_object_instance_refs) == 7
+
+
+@pytest.mark.parametrize(
+    ("filename", "password"),
+    [
+        (RESOURCES_PATH / "test_project-ets4-no_password.knxproj", None),
+        (RESOURCES_PATH / "test_project-ets4.knxproj", "Test"),
+    ],
+)
+def test_parse_project_ets4(filename, password):
+    """Test parsing of ETS4 project."""
+    with extract(filename, password) as knx_project_contents:
+        parser = XMLParser(knx_project_contents)
+        parser.parse()
+
+    assert len(parser.group_addresses) == 3
+    parsed_gas = {ga.address for ga in parser.group_addresses}
+    assert len(parsed_gas) == len(parser.group_addresses)
+    assert parsed_gas == {
+        "0/0/1",
+        "0/0/2",
+        "0/0/3",
+    }
+
+    assert len(parser.areas) == 1
+    assert len(parser.areas[0].lines) == 1
+    assert len(parser.areas[0].lines[0].devices) == 2
+    assert parser.areas[0].lines[0].devices[0].manufacturer_name == "MDT technologies"
+    assert parser.areas[0].lines[0].devices[1].manufacturer_name == "ABB"
+
+    assert len(parser.devices) == 2
+    assert parser.devices[0].individual_address == "0.0.1"
+    assert parser.devices[1].individual_address == "0.0.2"
 
 
 def test_parse_project_with_module_defs():
